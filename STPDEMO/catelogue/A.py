7b706f0557f43e075ce5300043a2db8eb0436cb6
Jani Pasha <jani.pasha@nexiilabs.com>
2019-01-07 15:09:59 +0530

    testing

---
+++ STPDEMO/catelogue/A.py
@@ -0,0 +1,11 @@
+class A:
+	def div(self):
+		"""
+		classifier:manage
+		section classifier:1 2
+		dependency:usercreation
+		dependent_is_built_in:True
+		path:Others/Task/Task5/Flas
+		"""
+		print "div"
+		return None
